d3edc8b7938663f1abb615d32cdc346ef86dc040
Andrew Usher <andrewusher00@gmail.com>
2019-08-16 07:25:44 -0500

    Migrate footer to styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import '../styles/_footer.styl'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px 0;
+
+  p {
+    color: #eee;
+    font-size: 18px;
+  }
+
+  a {
+    color: inherit;
+  }
+`
 
 const Footer = () => (
-  <footer>
-    <p>Made with love and code by <a href="http://andrewusher.co">Andrew Usher</a></p>
-  </footer>
+  <StyledFooter>
+    <p>
+      Made with love and code by <a href="http://andrewusher.co">Andrew Usher</a>
+    </p>
+  </StyledFooter>
 )
 
 export default Footer
